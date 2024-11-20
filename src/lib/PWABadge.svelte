f150a115e909c35bce8ecd5c88a7e2af90b1d15d
Michinobu Maeda <michinobumaeda@gmail.com>
2024-11-20 19:26:37 +0900

    chore: update Prettier configuration and clean up message files; refactor layout and PWABadge component

--- src/lib/PWABadge.svelte
+++ src/lib/PWABadge.svelte
@@ -1,47 +1,53 @@
 <script>
-	// @ts-nocheck
+  // @ts-nocheck
 
-	import { useRegisterSW } from 'virtual:pwa-register/svelte';
+  import { useRegisterSW } from "virtual:pwa-register/svelte";
 
-	// periodic sync is disabled, change the value to enable it, the period is in milliseconds
-	// You can remove onRegisteredSW callback and registerPeriodicSync function
-	const period = 10 * 60 * 1000;
+  // periodic sync is disabled, change the value to enable it, the period is in milliseconds
+  // You can remove onRegisteredSW callback and registerPeriodicSync function
+  const period = 60 * 60 * 1000;
 
-	/**
-	 * This function will register a periodic sync check every hour, you can modify the interval as needed.
-	 * @param swUrl {string}
-	 * @param r {ServiceWorkerRegistration}
-	 */
-	function registerPeriodicSync(swUrl, r) {
-		if (period <= 0) return;
+  /**
+   * This function will register a periodic sync check every hour, you can modify the interval as needed.
+   * @param swUrl {string}
+   * @param r {ServiceWorkerRegistration}
+   */
+  function registerPeriodicSync(swUrl, r) {
+    if (period <= 0) return;
 
-		setInterval(async () => {
-			if ('onLine' in navigator && !navigator.onLine) return;
+    setInterval(async () => {
+      if ("onLine" in navigator && !navigator.onLine) return;
 
-			const resp = await fetch(swUrl, {
-				cache: 'no-store',
-				headers: {
-					cache: 'no-store',
-					'cache-control': 'no-cache'
-				}
-			});
+      const resp = await fetch(swUrl, {
+        cache: "no-store",
+        headers: {
+          cache: "no-store",
+          "cache-control": "no-cache",
+        },
+      });
 
-			if (resp?.status === 200) await r.update();
-		}, period);
-	}
+      if (resp?.status === 200) await r.update();
+    }, period);
+  }
 
-	useRegisterSW({
-		onRegisteredSW(swUrl, r) {
-			if (period <= 0) return;
-			if (r?.active?.state === 'activated') {
-				registerPeriodicSync(swUrl, r);
-			} else if (r?.installing) {
-				r.installing.addEventListener('statechange', (e) => {
-					/** @type {ServiceWorker} */
-					const sw = e.target;
-					if (sw.state === 'activated') registerPeriodicSync(swUrl, r);
-				});
-			}
-		}
-	});
+  const { needRefresh, updateServiceWorker } = useRegisterSW({
+    onRegisteredSW(swUrl, r) {
+      if (period <= 0) return;
+      if (r?.active?.state === "activated") {
+        registerPeriodicSync(swUrl, r);
+      } else if (r?.installing) {
+        r.installing.addEventListener("statechange", (e) => {
+          /** @type {ServiceWorker} */
+          const sw = e.target;
+          if (sw.state === "activated") registerPeriodicSync(swUrl, r);
+        });
+      }
+    },
+  });
+
+  needRefresh.subscribe((v) => {
+    if (v) {
+      updateServiceWorker(true);
+    }
+  });
 </script>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,58 +1,61 @@
 <script>
-	import { ParaglideJS } from '@inlang/paraglide-sveltekit';
-	import { i18n } from '$lib/i18n';
-	import * as m from '$lib/paraglide/messages.js';
-
-	import PWABadge from '$lib/PWABadge.svelte';
-	import { version } from '$app/environment';
-	// import appLogo from '/favicon.svg';
-	import '../app.css';
-	let { children } = $props();
+  import { ParaglideJS } from "@inlang/paraglide-sveltekit";
+  import { version } from "$app/environment";
+  import { i18n } from "$lib/i18n";
+  import PWABadge from "$lib/PWABadge.svelte";
+  import "../app.css";
+  let { children } = $props();
 </script>
 
-<ParaglideJS {i18n}>
-	<div
-		class="flex flex-row justify-center
-    bg-lightSurfaceVariant dark:bg-darkSurfaceVariant"
-	>
-		<div
-			class="flex w-full max-w-screen-sm flex-col
-      bg-lightBackground text-lightOnBackground
-      dark:bg-darkBackground dark:text-darkOnBackground"
-		>
-			<div class="flex min-h-screen flex-col justify-between">
-				<header
-					class="flex
-      bg-lightPrimaryContainer text-lightOnPrimaryContainer
-      dark:bg-darkPrimaryContainer dark:text-darkOnPrimaryContainer"
-				>
-					<h1 class="flex flex-row p-1 text-2xl">
-						<img src="/favicon.svg" alt="Honkipass v5 Logo" class="mr-2 h-8 w-8" />
-						{m.title()} v5
-					</h1>
-				</header>
+<div
+  class="flex flex-row justify-center
+      bg-lightSurfaceVariant dark:bg-darkSurfaceVariant"
+>
+  <div
+    class="flex w-full max-w-screen-sm flex-col
+        bg-lightBackground dark:bg-darkBackground
+         text-lightOnBackground dark:text-darkOnBackground"
+  >
+    <div class="flex min-h-screen flex-col justify-between">
+      <header
+        class="flex
+            bg-lightPrimaryContainer dark:bg-darkPrimaryContainer
+            text-lightOnPrimaryContainer dark:text-darkOnPrimaryContainer"
+      >
+        <h1 class="flex flex-row p-1 text-2xl">
+          <img
+            src="/favicon.svg"
+            alt="Honkipass v5 Logo"
+            class="mr-2 h-8 w-8"
+          />
+          本気でパスワード v5
+        </h1>
+      </header>
 
-				<main class="mb-auto flex justify-center">
-					<div class="flex flex-col gap-4 px-1 pb-6 pt-4 sm:gap-8 sm:pt-8">
-						{@render children()}
-					</div>
-				</main>
-				<footer
-					class="flex flex-row gap-1 bg-lightSecondary px-4 py-1
-      text-base text-lightOnSecondary
-      dark:bg-darkSecondary dark:text-darkOnSecondary"
-				>
-					© 2024 Michinobu Maeda
-					<a
-						class="mx-2 text-lightInversePrimary underline
-        underline-offset-2 dark:text-darkInversePrimary"
-						href="https://github.com/MichinobuMaeda/honkipass5">Source</a
-					>
-					v{version}
-				</footer>
-			</div>
-		</div>
-	</div>
+      <main class="mb-auto flex justify-center">
+        <div class="flex flex-col gap-4 px-1 pb-6 pt-4 sm:gap-8 sm:pt-8">
+          <ParaglideJS {i18n}>
+            {@render children()}
+          </ParaglideJS>
+        </div>
+      </main>
+      <footer
+        class="flex flex-row gap-1 text-base px-4 py-1
+            bg-lightSecondary dark:bg-darkSecondary
+            text-lightOnSecondary dark:text-darkOnSecondary"
+      >
+        © 2024 Michinobu Maeda
+        <a
+          class="mx-2 underline underline-offset-2
+              text-lightInversePrimary dark:text-darkInversePrimary"
+          href="https://github.com/MichinobuMaeda/honkipass5"
+        >
+          GitHub
+        </a>
+        v{version}
+      </footer>
+    </div>
+  </div>
+</div>
 
-	<PWABadge />
-</ParaglideJS>
+<PWABadge />
